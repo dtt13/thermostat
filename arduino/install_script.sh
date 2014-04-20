bf32b10e9645689ee5fd55de65a49733224cbd3d
Derrick Tilsner <dtt13@case.edu>
2014-04-20 18:01:05 -0400

    changed install script to not use pip

--- arduino/install_script.sh
+++ arduino/install_script.sh
@@ -10,17 +10,13 @@ PIP=$PACK/bin/pip
 opkg update
 mkdir $PACK
 
-# install the PIL package
-echo
-echo "Installing PIL..."
-opkg install python-imaging-library
 
-# install easy_install and pip which is used for installing other packages
-echo
-echo "Installing pip..."
+# # install easy_install and pip which is used for installing other packages
+# echo
+# echo "Installing pip..."
 opkg install distribute
 opkg install python-openssl
-easy_install pip
+# easy_install pip
 
 # install virtualenv
 echo
@@ -32,17 +28,49 @@ virtualenv $PACK
 # install the web.py package
 echo
 echo "Installing web.py..."
-$PIP install web.py
+# $PIP install web.py
+cd $PACK
+wget http://webpy.org/static/web.py-0.37.tar.gz
+tar xvzf web.py-0.37.tar.gz
+cd $PACK/web.py-0.37
+$PYTHON setup.py install
+cd $PACK
+rm -rf $PACK/web.py-0.37.tar.gz $PACK/web.py-0.37
 
 # install suds
 echo
 echo "Installing suds..."
 $PIP install suds
+# cd $PACK
+# wget https://pypi.python.org/packages/source/s/suds/suds-0.3.4.tar.gz
+# tar xvzf suds-0.3.4.tar.gz
+# cd $PACK/suds-0.3.4
+# $PYTHON setup.py install
+# cd $PACK
+# rm -rf $PACK/suds-0.3.4.tar.gz $PACK/suds-0.3.4
 
-# install lxml
-# echo
-# echo "installing lxml..."
+# install the PIL package
+echo
+echo "Installing PIL..."
+opkg install python-imaging-library
+# cd $PACK
+# wget http://effbot.org/downloads/Imaging-1.1.7.tar.gz
+# tar xvzf Imaging-1.1.7.tar.gz
+# cd $PACK/Imaging-1.1.7
+# $PYTHON setup.py install
+# cd $PACK
+# rm -rf $PACK/Imaging-1.1.7.tar.gz $PACK/Imaging-1.1.7
+
+# install ElementTree
+echo
+echo "installing ElementTree..."
 # $PIP install lxml
+wget http://effbot.org/media/downloads/elementtree-1.2.6-20050316.tar.gz
+tar xvzf elementtree-1.2.6-20050316.tar.gz
+cd $PACK/elementtree-1.2.6-20050316
+$PYTHON setup.py install
+cd $PACK
+rm -rf $PACK/elementtree-1.2.6-20050316.tar.gz $PACK/elementtree-1.2.6-20050316
 
 echo
 echo "All Done."
